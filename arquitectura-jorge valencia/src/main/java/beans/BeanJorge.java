8b25253023d34cc248881fa42d603a3b47e5599d
usuario <usuario@usuario-PC>
2016-02-27 11:56:15 -0500

    taller 27-02-2016 JAVV

---
+++ arquitectura-jorge valencia/src/main/java/beans/BeanJorge.java
@@ -0,0 +1,35 @@
+package beans;
+
+public class BeanJorge {
+
+	private int x;
+	private int y;
+	private int suma;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSuma() {
+		return suma;
+	}
+
+	public void setSuma() {
+		this.suma = this.x+this.y;
+	}
+
+	
+
+}
